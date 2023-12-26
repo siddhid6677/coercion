e6ba69ce30de3fa5d7c3502ac5087a34e6023489
siddhid6677 <150324418+siddhid6677@users.noreply.github.com>
2023-12-26 12:24:46 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,180 @@
+var cl = console.log;
+
+var x = 10;
+var y = 10;
+
+var z = x + y;
+cl(z, typeof z);
+
+var z = x - y;
+cl(z, typeof z);
+
+var z = x * y;
+cl(z, typeof z);
+
+var z = x / y;
+cl(z, typeof z);
+
+var a = 10;
+var b = "10";
+
+var c = a + b;
+cl(c, typeof c);
+
+var c = a - b;
+cl(c, typeof c);
+
+var c = a * b;
+cl(c, typeof c);
+
+var c = a / b;
+cl(c, typeof c);
+
+var d = "20";
+var e = "30";
+
+var f = d + e;
+cl(f, typeof f);
+
+var f = d - e;
+cl(f, typeof f);
+
+var f = d * e;
+cl(f, typeof f);
+
+var f = d / e;
+cl(f, typeof f);
+
+var g = 10;
+var h = true;
+
+var i = g + h;
+cl(i, typeof i);
+
+var i = g - h;
+cl(i, typeof i);
+
+var i = g * h;
+cl(i, typeof i);
+
+var i = g / h;
+cl(i, typeof i);
+
+var m = 10;
+var j = false;
+
+var k = m + j;
+cl(k, typeof k);
+
+var k = m - j;
+cl(k, typeof k);
+
+var k = m * j;
+cl(k, typeof k);
+
+var g = "10";
+var h = true;
+
+var i = g + h;
+cl(i, typeof i);
+
+var i = g - h;
+cl(i, typeof i);
+
+var i = g * h;
+cl(i, typeof i);
+
+var i = g / h;
+cl(i, typeof i);
+
+var p = 50;
+var q;
+
+var s = p + q;
+cl(s, typeof s);
+
+var s = p - q;
+cl(s, typeof s);
+
+var s = p * q;
+cl(s, typeof s);
+
+var s = p / q;
+cl(s, typeof s);
+
+var r = "60";
+var t = null;
+
+var u = r + t;
+cl(u, typeof u);
+
+var u = r - t;
+cl(u, typeof u);
+
+var u = r * t;
+cl(u, typeof u);
+
+
+var A = 10;
+var B = "20"
+var C;
+var D = 10;
+var E = true;
+var F;
+var G = false;
+var I = 30;
+var H = null;
+
+var Z = A + B;
+cl(Z, typeof Z);
+
+var Z = A + B + C;
+cl(Z, typeof Z);
+
+var Z = A + B + C + D;
+cl(Z, typeof Z);
+
+var Z = A + B + C + D + E;
+cl(Z, typeof Z);
+
+var Z = A + B + C + D + E + F;
+cl(Z, typeof Z);
+
+var Z = A + B + C + D + E + F + G;
+cl(Z, typeof Z);
+
+var Z = A + B + C + D + E + F + G + I;
+cl(Z, typeof Z);
+
+var Z = A + B + C + D + E + F + G + I + H;
+cl(Z, typeof Z);
+
+var Z = A - B;
+cl(Z, typeof Z);
+
+var Z = A - B - C;
+cl(Z, typeof Z);
+
+var Z = A - B - C - D;
+cl(Z, typeof Z);
+
+var Z = A - B - C - D - E;
+cl(Z, typeof Z);
+
+var Z = A - B - C - D - E - F;
+cl(Z, typeof Z);
+
+var Z = A - B - C - D - E - F - G;
+cl(Z, typeof Z);
+
+var Z = A - B - C - D - E - F - G - I;
+cl(Z, typeof Z);
+
+var Z = A - B - C - D - E - F - G - I - H;
+cl(Z, typeof Z);
+
+var Z = A + B - C * D / E - F + G * I + H;
+cl(Z, typeof Z);
+
+var Z = A + B - C + D - E + F - G + I - H;
+cl(Z, typeof Z);
